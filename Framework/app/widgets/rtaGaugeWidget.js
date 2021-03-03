606036809feae18b6a3fc1cc344fa37888062911
Sam Niyazi <sam2003@niyazi.com>
2021-03-03 04:03:22 -0600

    Completed M03-06.

---
+++ Framework/app/widgets/rtaGaugeWidget.js
@@ -0,0 +1,23 @@
+﻿
+// 03/03/2021 03:57 am - SSN - [20210303-0355] - [001] - M03-06 - Widget creation
+
+'use strict';
+
+angular.module('app').directive('rtaGaugeWidget', [
+
+    function () {
+
+        return {
+
+            // Scope isherited from psWidgetBody
+
+            templateUrl: '/app/widgets/rtaGauge/rtaGaugeWidgetTemplate.html',
+
+            link: function (scope, element, attributes) {
+
+                scope.metric = scope.item.widgetSettings.metric;
+
+            }
+        }
+    }
+]);
