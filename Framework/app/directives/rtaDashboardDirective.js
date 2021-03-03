20c47104977c844d1d874effd748b504b5ad2806
Sam Niyazi <sam2003@niyazi.com>
2021-03-03 03:52:14 -0600

    Completed M03-04.

---
+++ Framework/app/directives/rtaDashboardDirective.js
@@ -0,0 +1,39 @@
+﻿
+// 03/03/2021 03:37 am - SSN - [20210303-0336] - [001] - M03-04 - Dashboard creation
+
+'use strict';
+
+angular.module('app').directive('rtaDashboard', ['$localStorage', function ($localStorage) {
+
+    return {
+        scope: {
+
+        },
+        template: '<ps-dashboard></ps-dashboard>',
+        link: function {
+
+            scope.gridsterOptions = {
+
+                columns: 12,
+                margins: [20, 20],
+                outerMargin: false,
+                pushing: true,
+                floating: true,
+                swapping:true
+            };
+
+
+            scope.widgetDefinitions = [];
+
+            scope.widgets = $localStorage.widgets || [];
+
+            scope.$watch('widgets', function () {
+
+                $localStorage.widgets = scope.widgets;
+
+            }, true);
+
+        }
+    }
+
+}]);
